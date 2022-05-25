b08097498249bf551c3992fd5412bebe2607f4ef
LHenriquerios <henriquerios4@gmail.com>
2022-05-25 20:40:26 -0300

    fix: message error login

--- src/middlewares/validateJoi.js
+++ src/middlewares/validateJoi.js
@@ -1,11 +1,20 @@
 const { BAD_REQUEST } = require('../statusCode');
 
-module.exports = (schemas) => (req, res, next) => {
+module.exports = (schemas) => (req, _res, next) => {
     const { error } = schemas.validate(req.body);
-    if (error && error.details[0].message.includes('is required')) {
-        next({ status: BAD_REQUEST, message: 'Some required fields are missing' });
+
+    if (error) {
+      switch (error.details[0].type) {
+        case 'any.required':
+          next({ status: BAD_REQUEST, message: 'Some required fields are missing' });
+          break;
+        case 'string.empty':
+          next({ status: BAD_REQUEST, message: 'Some required fields are missing' });
+          break;
+        default:
+            next({ status: BAD_REQUEST, message: error.details[0].message });
+      }
     }
-    if (error) next({ status: BAD_REQUEST, message: error.details[0].message });
 
     next();
 };
